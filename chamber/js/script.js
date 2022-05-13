7b6f47910f5624b09de7b0a6b54ac7c97fa1b374
Cryptum45 <bir19019@byui.edu>
2022-05-12 18:07:33 -0600

    Changes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -5,3 +5,11 @@ document.getElementById("currentYear").textContent = d.getFullYear();
 let lastUpdated = document.lastModified;
 
 document.getElementById("updatedDate").textContent = lastUpdated;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navmenu')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
