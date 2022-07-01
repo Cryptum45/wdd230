d5f17738d6ab622f7788cd7eb3ac767e232a8f8e
Cryptum45 <bir19019@byui.edu>
2022-06-30 18:50:03 -0600

    Changes wather again

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -38,19 +38,15 @@ function displayResults(weatherData){
 const windSpeed = document.querySelector("#wind.speed")
 let temp = parseFloat(document.querySelector("#current-temp").textContent);
 let speed = parseFloat(document.querySelector("#speed").textContent);
-let windchill = "";
 
 
-if (temp <= 50 && speed > 3) {
-  windchill = windChill(temp, speed);
-  windchill = `${windchill}&#176;F`;
-} else {
-  windchill = "N/A";
-}
+const windspeed = document.getElementById('wind').innerHTML;
 
-document.querySelector("#chill").innerHTML = windchill;
 
-function windChill(temp, speed) {
-  let windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-  return Math.round((windchill + Number.EPSILON) * 100) / 100;;
-}
+let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
+  if (temp <= 50 && windspeed > 3) {
+     windchill = Math.round(windchill);
+  } else {
+     windchill = "N/A";
+  }
+document.getElementById('windchill').innerHTML = windchill;
