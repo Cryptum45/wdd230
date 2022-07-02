1e69c5efdc0cf326422da5fc0fd4c85227ea3c8a
Cryptum45 <bir19019@byui.edu>
2022-07-02 14:44:22 -0600

    fixed windchill

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,9 +1,25 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector("#currentSpeed");
+const windChill = document.querySelector('#windChill');
+
+
+function calChill(windspeed, temp){
+
+
+let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
+  if (temp <= 50 && windspeed > 3) {
+     windchill = Math.round(windchill);
+  } else {
+     windchill = "N/A";
+  }
+
+  windChill.innerHTML = windchill
 
 
 
+}
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=42bce669996441380aed65928d42a37f';
 apiFetch(url);
@@ -14,7 +30,7 @@ async function apiFetch(apiURL){
         if (response.ok){
             const data = await response.json();
             displayResults(data)
-            console.log(jsObject);
+           
         }
         else{
             throw Error(await response.text());
@@ -25,6 +41,8 @@ async function apiFetch(apiURL){
 }
 
 function displayResults(weatherData){
+    calChill(weatherData.wind.speed.toFixed(1), weatherData.main.temp.toFixed(1))
+    windSpeed.innerHTML = weatherData.wind.speed.toFixed(1);
     currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -35,18 +53,3 @@ function displayResults(weatherData){
 
 
 }
-const windSpeed = document.querySelector("#wind.speed")
-let temp = parseFloat(document.querySelector("#current-temp").textContent);
-let speed = parseFloat(document.querySelector("#speed").textContent);
-
-
-const windspeed = document.getElementById('wind').innerHTML;
-
-
-let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
-  if (temp <= 50 && windspeed > 3) {
-     windchill = Math.round(windchill);
-  } else {
-     windchill = "N/A";
-  }
-document.getElementById('windchill').innerHTML = windchill;
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,5 +1,5 @@
 const temp = document.querySelector('#current-temp');
-const windspeed = document.getElementById('windspeed').innerHTML;
+const windspeed = document.getElementById('wind').innerHTML;
 
 
 let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
